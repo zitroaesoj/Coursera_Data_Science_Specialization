e6260a9dd2fd29ed00eb3d9aa42853f2108a8b70
Jose Ortiz <jose@mizacatecas.com>
2016-01-23 18:00:14 -0800

    Remove Unused File

--- jFunctionsWeek2Quiz.R
+++
@@ -1,7 +0,0 @@
-pollutantmean <- function(directory, pollutant, id = 1:332) {
-	myfiles <- list.files(path = directory, pattern="*.csv")
-	mytable <- lapply(paste(directory, myfiles[id], sep=""), read.csv, header = TRUE)
-	mycombineddf <- do.call(rbind , mytable )
-	dim(mycombineddf )
-	mean(mycombineddf[,pollutant], na.rm=TRUE)
-}
